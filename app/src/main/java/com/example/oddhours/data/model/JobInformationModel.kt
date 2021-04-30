a5b7aa2ad1abf59e2c5273d345ba61da90ca3a1b
Praise <epraise17@yahoo.com>
2021-04-30 18:03:37 -0400

    generate dummy list using models now
    ✓ dummy function now uses JobRepository to create jobs
    ✗ more work incorporating an information model to both the job model and repository

--- app/src/main/java/com/example/oddhours/data/model/JobInformationModel.kt
+++ app/src/main/java/com/example/oddhours/data/model/JobInformationModel.kt
@@ -1,5 +1,6 @@
 package com.example.oddhours.data.model
 
+import java.time.LocalDate
 import java.time.LocalTime
 
-data class JobInformationModel(val startTime: LocalTime)
+data class JobInformationModel(val date: LocalDate, val startTime: LocalTime, val endTime: LocalTime)
--- app/src/main/java/com/example/oddhours/data/repository/JobRepository.kt
+++ app/src/main/java/com/example/oddhours/data/repository/JobRepository.kt
@@ -2,11 +2,25 @@ package com.example.oddhours.data.repository
 
 import com.example.oddhours.data.model.JobModel
 
-class JobRepository {
+class JobRepository() {
 
-    private var JobModel: JobModel? = null
+    /**
+     * planning to use this to store the list of jobs when we can access it from the DB
+     */
+    private var jobModelList: List<JobModel>? = null
+
+    /**
+     * this will be the method to get the jobs and store in jobModelList
+     */
+//    fun getJobs(): List<JobModel> {
+//
+//    }
+
+    fun createJob(jobName: String, jobLocation: String, jobInfo: String): JobModel {
+        return JobModel(jobName, jobLocation, jobInfo)
+    }
 
     fun getTotalHoursForWeek(): String {
-        return this.JobModel.toString()
+        return "This week: X hours"
     }
 }
