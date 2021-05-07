b4d4c574c7099bf9b3cab07fcc6eca9377dfcc93
Praise <epraise17@yahoo.com>
2021-05-06 22:58:17 -0400

    pull request feedback
    ✓ change models to fit database structure
    ✓ rename JobInformationModel -> ShiftsModel

--- app/src/main/java/com/example/oddhours/data/repository/JobRepository.kt
+++ app/src/main/java/com/example/oddhours/data/repository/JobRepository.kt
@@ -1,23 +1,19 @@
 package com.example.oddhours.data.repository
 
-import com.example.oddhours.data.model.JobInformationModel
+import com.example.oddhours.data.model.ShiftsModel
 import com.example.oddhours.data.model.JobModel
 
 class JobRepository() {
 
     /**
-     * planning to use this to store the list of jobs when we can access it from the DB
+     * store the list of jobs
      */
     private var jobModelList: List<JobModel>? = null
 
-    private var jobInformation: JobInformationModel? = null
-
     /**
-     * TODO: add a new job to the DB. might need to double check the return type
+     * store the list of shifts
      */
-    fun addNewJob(jobName: String, jobLocation: String, jobInfo: String): JobModel {
-        return JobModel(jobName, jobLocation, jobInfo)
-    }
+    private var shiftsList: List<ShiftsModel>? = null
 
     /**
      * TODO: this method should make use of the return object / values from DB (instead of taking in jobName and jobLocation)
@@ -25,15 +21,22 @@ class JobRepository() {
      * MainActivity after we connect to the DB and query it to get all current jobs the user has stored. the return list is
      * what we feed into the recyclerview adapter!
      */
-    fun createJob(jobName: String, jobLocation: String): JobModel {
-        return JobModel(jobName, jobLocation, getTotalHoursForWeek(jobName))
+    fun buildJobList(jobName: String, jobLocation: String): JobModel {
+        return JobModel(1, jobName, jobLocation, getTotalHoursForWeek(jobName))
     }
 
     /**
-     * TODO: this method will be responsible for calculating total hours in the current week cycle
+     * TODO: this method will be responsible for calculating total hours in the current week cycle for a specific job
      * might be smart to pass in something else as parameter instead of jobName (to make lookup faster)
      */
     private fun getTotalHoursForWeek(jobName: String): String {
         return "This week: X hours"
+    }
+
+    /**
+     * TODO: add a new job to the DB (and possibly to jobModelList). might need to double check the return type
+     */
+    fun addNewJob(jobName: String, jobLocation: String, jobInfo: String): JobModel {
+        return JobModel(1, jobName, jobLocation, jobInfo)
     }
 }
