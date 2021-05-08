2ae0ce48aa3a654a3f5cdcbffda20596c6619018
Meet Pandya <pandyama2@gmail.com>
2021-05-08 18:24:00 -0400

    added insert and get functions
    Insert and get functions for jobs and shifts tables.

--- app/src/main/java/com/example/oddhours/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/oddhours/database/DatabaseHelper.kt
@@ -10,32 +10,42 @@ class DatabaseHelper(context: Context):
     SQLiteOpenHelper(context, dbName, null, 1){
 
     val sqlCreateTable_job = "CREATE TABLE IF NOT EXISTS $jobTable (" +
-            "$job_COL_0 INTEGER PRIMARY KEY, " +
-            "$job_COL_1 TEXT, " +
-            "$job_COL_2 TEXT);"
+            "$job_ID_COL_1 INTEGER PRIMARY KEY, " +
+            "$job_Name_COL_2 TEXT, " +
+            "$job_Location_COL_3 TEXT);"
     val sqlCreateTable_hours = "CREATE TABLE IF NOT EXISTS $hoursTable (" +
-            "$hours_COL_0 INTEGER PRIMARY KEY, " +
-            "$hours_COL_1 TEXT, " +
-            "$hours_COL_2 INTEGER, " +
-            "$hours_COL_3 TEXT, " +
-            "$hours_COL_4 TEXT, " +
-            "$hours_COL_5 INTEGER, " +
-            "FOREIGN KEY($hours_COL_2) REFERENCES $jobTable($job_COL_0));"
+            "$shift_ID_COL_1 INTEGER PRIMARY KEY, " +
+            "$shift_Date_COL_2 TEXT, " +
+            "$job_ID_COL_3 INTEGER, " +
+            "$start_Time_COL_4 TEXT, " +
+            "$end_Time_COL_5 TEXT, " +
+            "$hours_Worked_COL_6 INTEGER, " +
+            "FOREIGN KEY($job_ID_COL_3) REFERENCES $jobTable($job_ID_COL_1));"
+
+    val getJobs = "SELECT * FROM $jobTable"
+
+    //JobModel (JobId, JobName, JobLocation)
+    //ShiftsModel (ShiftID, ShiftDate, JobID, StartTime, EndTime, HoursWorked)
 
     companion object {
         val dbName = "oddHours"
         val jobTable = "Job"
-        val job_COL_0 = "JobID"
-        val job_COL_1 = "JobName"
-        val job_COL_2 = "JobLocation"
+        val job_ID_COL_1 = "JobID"
+        val job_Name_COL_2 = "JobName"
+        val job_Location_COL_3 = "JobLocation"
         val hoursTable = "Hours"
-        val hours_COL_0 = "ShiftID"
-        val hours_COL_1 = "ShiftDate"
-        val hours_COL_2 = "JobID"
-        val hours_COL_3 = "StartTime"
-        val hours_COL_4 = "EndTime"
-        val hours_COL_5 = "HoursWorked"
+        val shift_ID_COL_1 = "ShiftID"
+        val shift_Date_COL_2 = "ShiftDate"
+        val job_ID_COL_3 = "JobID"
+        val start_Time_COL_4 = "StartTime"
+        val end_Time_COL_5 = "EndTime"
+        val hours_Worked_COL_6 = "HoursWorked"
     }
+
+    // Supported Data Types by SQLite
+    //  NULL, INTEGER, REAL, TEXT, BLOB
+    // so, we will have to store, dates and times as strings
+    // "YYYY-MM-DD" and "HH:MM:SS.SSS"
 
     override fun onCreate(db: SQLiteDatabase?) {
         db!!.execSQL(sqlCreateTable_job)
@@ -45,6 +55,43 @@ class DatabaseHelper(context: Context):
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS $dbName")
         onCreate(db)
+    }
+
+    fun insertJob(jobID: Int, jobName: String, jobLocation: String): Long{
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(job_ID_COL_1, jobID)
+        contentValues.put(job_Name_COL_2, jobName)
+        contentValues.put(job_Location_COL_3, jobLocation)
+
+        return db.insert(jobTable, null, contentValues)
+    }
+
+    fun insertShift(shiftID: Int, shiftDate: String, jobID: Int, start: String, end: String): Long{
+        //First calculate the hours worked, from the entered Start and End time
+        // Then push the data to SQLite
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(shift_ID_COL_1, shiftID)
+        contentValues.put(shift_Date_COL_2, shiftID)
+        contentValues.put(job_ID_COL_3, shiftID)
+        contentValues.put(start_Time_COL_4, shiftID)
+        contentValues.put(end_Time_COL_5, shiftID)
+        contentValues.put(hours_Worked_COL_6, shiftID)
+
+        return db.insert(hoursTable, null, contentValues)
+    }
+
+    fun getJobs(): Cursor {
+        val db = this.writableDatabase
+        val res = db.rawQuery(getJobs, null)
+        return res
+    }
+
+    fun getShiftsForjob(shiftID: Int): Cursor{
+        val db = this.writableDatabase
+        val res = db.rawQuery("SELECT * FROM $hoursTable WHERE $job_ID_COL_3 = '$shiftID'", null)
+        return res
     }
 
 }
