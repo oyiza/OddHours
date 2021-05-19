225bf60311d5377cbbc67d5c93806b325d80551f
Meet Pandya <pandyama2@gmail.com>
2021-05-19 19:41:44 -0400

    Popup dialog for entering shifts

--- app/src/main/java/com/example/oddhours/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/oddhours/ui/home/HomeAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.oddhours.ui.home
 
+import android.app.TimePickerDialog
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +9,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.oddhours.R
 import com.example.oddhours.data.model.JobModel
+import kotlinx.android.synthetic.main.addshift.view.*
 import kotlinx.android.synthetic.main.job_row.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class HomeAdapter(private val jobList: List<JobModel>) : RecyclerView.Adapter<HomeAdapter.JobViewHolder>() {
+/**
+ * HomeAdapter now also takes in the context as a parameter in the constructor
+ * context is required for the Alert Dialog
+ */
+
+class HomeAdapter(private val jobList: List<JobModel>, val context: Context) : RecyclerView.Adapter<HomeAdapter.JobViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.job_row, parent, false)
@@ -40,6 +51,45 @@ class HomeAdapter(private val jobList: List<JobModel>) : RecyclerView.Adapter<Ho
                 "You clicked button of : ${holder.jobName.text}",
                 Toast.LENGTH_LONG
             ).show()
+
+            /**
+             * below code is for popup dialog and the respective on button click listeners
+             */
+                val mDialogView = LayoutInflater.from(context).inflate(R.layout.addshift, null)
+                val mBuilder = AlertDialog.Builder(context)
+                    .setView(mDialogView)
+                    .setTitle("Add a Shift")
+                val mAlertDialog = mBuilder.show()
+
+                mDialogView.startTimeBTN.setOnClickListener{
+                    val cal = Calendar.getInstance()
+                    val timeSetListener = TimePickerDialog.OnTimeSetListener{
+                            timePicker, hour, minute ->
+                        cal.set(Calendar.HOUR_OF_DAY, hour)
+                        cal.set(Calendar.MINUTE, minute)
+                        mDialogView.startTimeTV.text = SimpleDateFormat("HH:mm").format(cal.time)
+                    }
+
+                    TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(
+                        Calendar.MINUTE), false).show()
+                }
+
+                mDialogView.endTimeBTN.setOnClickListener{
+                    val cal = Calendar.getInstance()
+                    val timeSetListener = TimePickerDialog.OnTimeSetListener{
+                            timePicker, hour, minute ->
+                        cal.set(Calendar.HOUR_OF_DAY, hour)
+                        cal.set(Calendar.MINUTE, minute)
+                        mDialogView.endTimeTV.text = SimpleDateFormat("HH:mm").format(cal.time)
+                    }
+
+                    TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(
+                        Calendar.MINUTE), false).show()
+                }
+
+                mDialogView.cancelBTN.setOnClickListener{
+                    mAlertDialog.dismiss()
+                }
         }
 
         // onClick listener for the card - in case we want to click the card and open the job details
