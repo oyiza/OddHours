b343e681cac2076d30e81a818127b279de971f5c
Meet Pandya <pandyama2@gmail.com>
2021-06-20 16:40:15 -0400

    Added layout for Edit/Delete shift & set onLongClickListener for shift card

--- app/src/main/java/com/example/oddhours/ui/shifts/ChildAdapter.kt
+++ app/src/main/java/com/example/oddhours/ui/shifts/ChildAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.oddhours.ui.shifts
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.oddhours.R
+import com.example.oddhours.data.model.JobModel
 import com.example.oddhours.data.model.ShiftsModel
+import com.example.oddhours.ui.home.HomeAdapter
+import kotlinx.android.synthetic.main.dialog_edit_delete_job.view.*
 import kotlinx.android.synthetic.main.item_shift_detail.view.*
 
 class ChildAdapter(private var shiftsList: List<ShiftsModel>, val context: Context): RecyclerView.Adapter<ChildAdapter.ViewHolder>() {
@@ -27,6 +33,15 @@ class ChildAdapter(private var shiftsList: List<ShiftsModel>, val context: Conte
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val shifts = shiftsList[position]
         holder.bindShifts(shifts)
+
+        holder.itemView.setOnLongClickListener{
+            val mDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_delete_shift, null)
+            val mBuilder = AlertDialog.Builder(context)
+                .setView(mDialogView)
+            mBuilder.show()
+
+            return@setOnLongClickListener true
+        }
     }
 
     override fun getItemCount() = shiftsList.size
